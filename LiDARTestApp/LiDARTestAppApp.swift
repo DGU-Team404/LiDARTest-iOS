b9d099f57418d55390775a5ec015eac437f68dca
Doyeon Kim <ehdus022191@dgu.ac.kr>
2026-03-06 15:41:51 +0900

    init: initial Comiit

---
+++ LiDARTestApp/LiDARTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiDARTestAppApp.swift
+//  LiDARTestApp
+//
+//  Created by 김도연 on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct LiDARTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
